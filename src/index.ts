8433ca77b46a8a249420556c78d04563cab3b429
Joshua Weber <josh@joshuaw.de>
2022-04-04 22:17:19 +0200

    :sparkles: Adds table print option

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createSpinner } from 'nanospinner';
 import chalk from 'chalk';
 import commandLineArgs from 'command-line-args';
 import { fetchTopStories, getMultipleItems } from './utils.js';
+import { printTable } from 'console-table-printer';
 
 const printItemsToConsole = (start: number, end: number, slicedItems: any[]) => {
   console.log(`+---`);
@@ -19,8 +20,26 @@ const printItemsToConsole = (start: number, end: number, slicedItems: any[]) => 
   }
 };
 
+const printTableToConsole = (start: number, end: number, slicedItems: any[]) => {
+  let articles = [];
+  for (let i = start; i < end; i++) {
+    const title = slicedItems[i].title;
+    let content;
+    if (slicedItems[i].url) {
+      content = `URL: ${slicedItems[i].url}`;
+    } else if (slicedItems[i].text) {
+      content = `Text: ${slicedItems[i].text}`;
+    }
+    articles.push({
+      title: title,
+      content: content,
+    });
+  }
+  printTable(articles);
+};
+
 // @ts-ignore
-const prettyPrintTopItems = async (start: number, end: number): Promise<any> => {
+const prettyPrintTopItems = async (start: number, end: number, tablePrint: boolean): Promise<any> => {
   if (start >= end) {
     console.error('Start parameter must be less than end parameter');
     return;
@@ -31,15 +50,20 @@ const prettyPrintTopItems = async (start: number, end: number): Promise<any> => 
   const slicedItems = await getMultipleItems(data.slice(start, end));
   spinner.clear();
   spinner.reset();
-  printItemsToConsole(start, end, slicedItems);
+  if (tablePrint) {
+    printTableToConsole(start, end, slicedItems);
+  } else {
+    printItemsToConsole(start, end, slicedItems);
+  }
 };
 const optionDefinitions = [
   { name: 'from', alias: 'f', type: Number, defaultValue: 0 },
   { name: 'to', alias: 't', type: Number, defaultValue: 3 },
+  { name: 'table', alias: 'b', type: Boolean, defaultValue: 'false' },
 ];
 
-const { from, to } = commandLineArgs(optionDefinitions, {
+const { from, to, table } = commandLineArgs(optionDefinitions, {
   partial: true,
   caseInsensitive: true,
 });
-prettyPrintTopItems(from, to);
+prettyPrintTopItems(from, to, table);
